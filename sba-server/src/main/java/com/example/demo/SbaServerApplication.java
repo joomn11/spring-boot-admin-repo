33b4d8db31dfab46ade640e92b63482f2eefc6bd
ej <joomn11@gmail.com>
2021-07-19 12:20:09 +0900

    sba-server - interception request response 설정 추가

--- sba-server/src/main/java/com/example/demo/SbaServerApplication.java
+++ sba-server/src/main/java/com/example/demo/SbaServerApplication.java
@@ -1,18 +1,34 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.web.client.InstanceExchangeFilterFunction;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableAutoConfiguration
 public class SbaServerApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SbaServerApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SbaServerApplication.class, args);
 	}
 
+	
+	@Bean
+	public InstanceExchangeFilterFunction auditLog() {
+		return (instance, request, next) -> next.exchange(request).doOnSubscribe((s) -> {
+			if (HttpMethod.DELETE.equals(request.method()) || HttpMethod.POST.equals(request.method())) {
+				log.info("{} for {} on {}", request.method(), instance.getId(), request.url());
+			}
+		});
+	}
 }
