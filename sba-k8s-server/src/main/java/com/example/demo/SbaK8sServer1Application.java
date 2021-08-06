0827ad42ae7c2cc3d5688e944cb0a6783651b128
ej <joomn11@gmail.com>
2021-08-06 13:44:29 +0900

    add k8s spring-cloud-config setting

--- sba-k8s-server/src/main/java/com/example/demo/SbaK8sServer1Application.java
+++ sba-k8s-server/src/main/java/com/example/demo/SbaK8sServer1Application.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
@@ -11,6 +12,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @EnableAdminServer
 @EnableScheduling
 @EnableDiscoveryClient
+@EnableConfigServer
 public class SbaK8sServer1Application {
 
 	public static void main(String[] args) {
